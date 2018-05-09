621d0246737e4934b66d9d33755d880b86d75ac5
elgatovielma <vielmagalberto@gmail.com>
2018-05-08 22:36:31 -0400

    Initial commit

---
+++ app/src/main/java/com/example/keyboardsample/ListViewLoader.java
@@ -0,0 +1,33 @@
+package com.example.keyboardsample;
+
+import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListViewLoader extends ListActivity
+        implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+}
